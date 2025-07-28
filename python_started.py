df2f7c0c46939ce6be5a505181441dd8806f678d
CuongPT <cuongphamtuan2003@gmail.com>
2025-07-28 11:14:53 +0700

    Update file name. Basic python learnings

---
+++ python_started.py
@@ -0,0 +1,35 @@
+print("Hello, World!\nThis is CuongPT24 speaking!")
+
+#Indentation example
+if True:
+    print("This is indented code block.")
+else:
+    print("This is not executed.")  
+
+#Variable assignment
+name = "CuongPT24"
+age = 24
+print("Name:", name)
+print("Age:", age)
+
+#Variable reassignment
+age = 25.0
+print("Updated Age:", age)
+
+#Type checking
+x = 5
+y = "CuongPT24"
+print(type(x))
+print(type(y))
+
+#Variable rules
+# 1. Cannot start with a number, example "1st_variable" is invalid
+#    but "variable1" , "_1st_variable" are valid
+# 2. Cannot contain spaces, example "my variable" is invalid
+#    but "my_variable" is valid
+# 3. Cannot use special characters except underscore (_), 
+#    example "my-variable" is invalid
+#    but "my_variable" is valid
+# 4. Case-sensitive, example "myVariable" and "myvariable" are different variables
+# 5. Cannot use Python reserved keywords, example "def", "class", "if", etc. are invalid
+
